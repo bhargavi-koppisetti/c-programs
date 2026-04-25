77c1b8b631fdb0ec0e3121309249c635e625b1d2
bhargavi-koppisetti <bhargavikoppisetti06@gmail.com>
2026-04-25 11:59:34 +0530

    Add files via upload

---
+++ lower case.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter the character\n");
+    scanf("%c",&ch);
+    int res=(ch>='a')&&(ch<='z');
+    printf("%c is a lower case alphabet i.e is:%d",ch,res);
+}
