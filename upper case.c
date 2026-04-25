899d93c8ec76054a21f8e2cece0819a846d72c08
bhargavi-koppisetti <bhargavikoppisetti06@gmail.com>
2026-04-25 11:59:01 +0530

    Add files via upload

---
+++ upper case.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter the character\n");
+    scanf("%c",&ch);
+    int res=(ch>='A')&&(ch<='Z');
+    printf("%c is a upper case alphabet i.e is:%d",ch,res);
+}
