c4a346ca1cf08996171aa51ff62ff90b828ce8cd
bhargavi-koppisetti <bhargavikoppisetti06@gmail.com>
2026-04-25 12:47:38 +0530

    Add files via upload

---
+++ party.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int tea, candy;
+    printf("Enter the amount of tea and candy:\n");
+    scanf("%d %d", &tea, &candy);
+
+    if (tea <= 5 || candy <= 5)
+    {
+        printf("bad=0");
+    }
+    else if (tea >= 2 * candy || candy >= 2 * tea)
+    {
+        printf("great=2");
+    }
+    else
+    {
+        printf("good=1");
+    }
+
+    return 0;
+}
